3a738dd56437adb4d26c7468820694651e3d9741
Shan <shanganegoda96@gmail.com>
2024-06-13 22:57:54 +0800

    completed header with responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import Logo from '../../assets/logo.svg'
+import NavIcon from '../../assets/nav-menu-icon.svg'
+import './Header.css'
 
 function NavBar() {
   return (
-    <div>
-      <img className='logo'></img>
+    <div className='header'>
+      <img className='logo' src={NavIcon} height={60}></img>
       <ul className='main-nav'>
-        <li>About</li>
-        <li>Work</li>
-        <li>Contact</li>
+        <li>ABOUT</li>
+        <li>WORK</li>
+        <li>CONTACT</li>
       </ul>
+      <img className='nav-menu-icon' src={NavIcon} height={60}></img>
     </div>
   )
 }
