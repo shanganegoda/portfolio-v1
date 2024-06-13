45fb965243d7e35c24da7b3ed35b1a05523ed5bd
Shan <shanganegoda96@gmail.com>
2024-06-13 12:26:23 +0800

    intial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+      <img className='logo'></img>
+      <ul className='main-nav'>
+        <li>About</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar
