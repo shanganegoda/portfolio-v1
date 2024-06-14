025103e9532be00fc4c681a079705696d97ad580
Shan <shanganegoda96@gmail.com>
2024-06-14 16:58:04 +0800

    implemented hero section for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
 
 function App() {
   return (
     <div>
       <Header/>
+      <Hero/>
     </div>
   )
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Hero.css'
+import ProPic from '../../assets/profile picture.jpg' 
+
+function Hero() {
+  return (
+    <section className='hero-section'>
+        <div className='wrapper'>
+            <h1>SHAN GANEGODA</h1>
+            <div className='profile'>
+                <p>Full Stack Developer with a passion to create awesome web applications.</p>
+                <p>Loves collaboration, learning and growing.</p>
+                <div>
+                <img className='pro-pic' src={ProPic} height={484} width={345}></img>
+                <p>Pasir Ris, Singapore.</p>
+                </div> 
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
